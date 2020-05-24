9e7b617c38ca644471107b6d422c74494652a957
thilina3045 <56603133+thilina3045@users.noreply.github.com>
2020-05-24 13:38:32 +0530

    Update question2.scala

--- question2.scala
+++ question2.scala
@@ -1,5 +1,5 @@
 object Volume extends App{
 
-def vol(radius:Double):Double: 4.00/3.00*math.pi*radius*radius*radius
+def vol(radius:Double):Double= 4.00/3.00*math.pi*radius*radius*radius
 printf(radius(5));
 }
